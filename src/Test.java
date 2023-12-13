8c05c679363f34f5ed9346addaa209ca69f22717
LocTM <tranminhloc.anhk@gmail.com>
2023-12-13 23:26:08 +0700

    done

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+
+        student.setName("Alice");
+        student.setClasses("C01");
+
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("Classes: " + student.getClasses());
+    }
+}
